385be32d6afc7f8cc0d2fa4407b2f282aad07d66
Eric Gonzalez <eric.g.gonzalez-1@ou.edu>
2016-09-12 19:27:16 -0500

    first commit

---
+++ src/Sample.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @author CS2334.  Modified by: ?????
+ * <P>
+ * Date: 2016-09-10 <BR>
+ * Project 1
+ * <P>
+ * This class represents individual, real-valued samples.  This class
+ * explicitly addresses the fact that some samples are invalid.
+ *
+ */
+
+public class Sample {
+	/** The observed value.  */
+	private double value;
+	
+	/** Indicates whether the observation is a valid one */
+	private boolean valid;
+	
+	// TODO: complete the implementation
+	
+}
